2ae0d20887e47296052f99f5df7cfb1ac0a73dac
Wesley Corner <wescorner@gmail.com>
2022-07-07 03:53:27 +0000

    sends jpgs as base64 data

--- server.js
+++ server.js
@@ -7,18 +7,27 @@ server.on("connection", (client) => {
 	console.log("client connected");
 	client.setEncoding("utf8");
 	client.on("data", (data) => {
-		fs.readFile(`./files/${data}`, "utf-8", (err, fileData) => {
-			if (err) {
-				client.write("ERROR: Incorrect file location!");
-			} else {
-				client.write(`Contents of ${data}: ${fileData}`);
-			}
-		});
+		if (data.substring(data.length - 3) === "txt") {
+			fs.readFile(`./files/${data}`, "utf-8", (err, fileData) => {
+				if (err) {
+					client.write("ERROR: Incorrect file location!");
+				} else {
+					client.write(`Contents of ${data}: ${fileData}`);
+				}
+			});
+		}
+		if (data.substring(data.length - 3) === "jpg") {
+			fs.readFile(`./files/${data}`, "base64", (err, fileData) => {
+				if (err) {
+					client.write("ERROR: Incorrect file location!");
+				} else {
+					client.write(Buffer.from(fileData).toString("base64"));
+				}
+			});
+		}
 	});
 });
 
 server.listen(3000, () => {
 	console.log("listening on port 3000");
 });
-
-//server will look for file with directory specified by the client
